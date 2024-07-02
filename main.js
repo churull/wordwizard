f30046b72bdd79eff95c35ab4bc90b2045b3b80c
churull <101821316+churull@users.noreply.github.com>
2024-07-02 17:17:11 +0700

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-const navbarBtn = document.querySelector('.navbar__icon')
-const navbar = document.querySelector('.navbar__mobile')
-navbarBtn.addEventListener('click', ()=>{
-    navbar.classList.toggle('active')
-})
